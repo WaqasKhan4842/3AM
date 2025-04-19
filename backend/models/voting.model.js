9df6b5e50647313c02f1c8d416785a9cf54f8376
WaqasKhan4842 <wkhh70709@gmail.com>
2025-04-19 10:11:56 +0500

    first commit

--- backend/models/voting.model.js
+++
@@ -1,7 +0,0 @@
-const voteSchema = new mongoose.Schema({
-    performance: { type: mongoose.Schema.Types.ObjectId, ref: "Performance", required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  }, { timestamps: true });
-  
-  export default mongoose.model("Vote", voteSchema);
-  
